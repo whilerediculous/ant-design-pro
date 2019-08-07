3e2dbc52904d56e00bef2325a4b81334fb3bd4fa
whilerediculous <sunspot.light@gmail.com>
2019-08-07 15:41:31 +0000

    graph change

--- src/utils/graph.js
+++ src/utils/graph.js
@@ -1,6 +1,5 @@
 /*
 Copyright (c) 2019 Tencent Inc.
-Author: admondfang <admondfang@tencent.com>
 Desc: class Graph
 */
 
@@ -14,7 +13,7 @@ class AdjvexNode {
 }
 
 function getNamedLinksContainTailEdge(links) {
-  const fullLinks = Object.assign([], ...links);
+  const fullLinks = Object.assign([], links);
   const allFrom = [];
   const allTo = [];
   fullLinks.forEach(item => {
@@ -47,7 +46,7 @@ function getUniqueNodes(edges) {
 function getNodeIndegreeFromAdjlist(nodeName, adjList) {
   let degree = 0;
 
-  Object.keys().forEach(key => {
+  Object.keys(adjList).forEach(key => {
     let node = adjList[key].next;
     while (node) {
       if (node.node === nodeName) {
@@ -80,7 +79,7 @@ function getNamedLinks(edges, deployments) {
 /*
  return [{source: xx, target: xx},]
 */
-function getValidLinks(vedge) {
+function getNonNullLinks(vedge) {
   const links = [];
   vedge.forEach(edge => {
     if (edge.from && edge.to) {
@@ -91,14 +90,16 @@ function getValidLinks(vedge) {
 }
 
 function getEdgeIndex(edges, fromKey) {
-  return edges.findIndex(index => edges[index].from && edges[index].from === fromKey);
+  return edges.findIndex(edge => edge.from && edge.from === fromKey);
 }
 
-// get_graph_data(route_list,deploy_info,['version', 'address', 'message'], ['color'])
+/*
+remove edges which  in-degree is 0,push into vec , and  loop
+*/
 function getDeepVec(routeList, deployInfos) {
   const deepVec = [];
   const namedEdges = getNamedLinks(routeList, deployInfos);
-  let edgeList = Object.assign([], { ...namedEdges });
+  let edgeList = getNamedLinksContainTailEdge(namedEdges);
   while (1) {
     const stack = [];
     const adjList = [];
@@ -119,17 +120,13 @@ function getDeepVec(routeList, deployInfos) {
       const degree = getNodeIndegreeFromAdjlist(key, adjList);
       inDegree[key] = degree;
     });
-    const deleteIndexs = [];
-    // eslint-disable-next-line no-loop-func
     Object.keys(inDegree).forEach(key => {
       if (inDegree[key] === 0) {
         stack.push(key);
-        const index = getEdgeIndex(edgeList, key);
-        deleteIndexs.push(index);
       }
     });
 
-    edgeList = edgeList.filter(index => deleteIndexs.indexOf(index) < 0);
+    edgeList = edgeList.filter(element => stack.indexOf(element.from) < 0);
     if (stack.length === 0) {
       return deepVec;
     }
@@ -137,7 +134,7 @@ function getDeepVec(routeList, deployInfos) {
   }
 }
 
-/**
+/*
  * @construct param elemnets_info [{name:'',color:'',version:'',address:'',message:''},...]
  * @construct param vedge edge array
  */
@@ -155,39 +152,51 @@ function getObjValueByKey(indexKey, field, obj) {
 /*
 @in_vec  graph in degree vector
 @elemnets_info   [{}],gerneral node atrribute
-@tip_keys  [] keys part of  elemnets_info and need show in tip
-@style_keys  current value ‘color’
+@ tip_keys  [] keys part of  elemnets_info and need show in tip
+        @style_keys  current value ‘color’
 */
-function getGraphData(routes, elementsInfos, tipKeys, styleKeys) {
-  const vedge = getNamedLinks(routes, elementsInfos);
-  const links = getValidLinks(vedge);
-  const inDegreeVec = getDeepVec(vedge);
+export default function getGraphData(routes, elementsInfos, tipKeys, styleKeys) {
   const data = [];
-  inDegreeVec.forEach((item, inDegree) => {
-    item.forEach((node, rowIndex) => {
-      const values = [];
-      const styles = {};
-      tipKeys.forEach(tipName => {
-        const tipValue = getObjValueByKey(node, tipName, elementsInfos);
-        if (tipValue) {
-          values.push({ name: tipName, value: tipValue });
+  let links = [];
+  let err = '';
+  try {
+    const vedge = getNamedLinks(routes, elementsInfos);
+    links = getNonNullLinks(vedge);
+    const inDegreeVec = getDeepVec(routes, elementsInfos);
+
+    inDegreeVec.forEach((item, inDegree) => {
+      item.forEach((node, rowIndex) => {
+        const values = [];
+        const styles = {};
+        tipKeys.forEach(tipName => {
+          const tipValue = getObjValueByKey(node, tipName, elementsInfos);
+          if (tipValue) {
+            values.push({ name: tipName, value: tipValue });
+          }
+        });
+
+        styleKeys.forEach(styleKey => {
+          const styleValue = getObjValueByKey(node, styleKey, elementsInfos);
+          if (styleValue) {
+            styles[styleKey] = styleValue;
+          }
+        });
+        let fact = 1;
+        if (item.length > 0) {
+          fact = 6 / (item.length + 1);
         }
-      });
-      styleKeys.forEach(styleKey => {
-        const styleValue = getObjValueByKey(node, styleKey, elementsInfos);
-        if (styleValue) {
-          styles[styleKey] = styleValue;
-        }
-      });
-      data.push({
-        name: node,
-        x: 70 * (inDegree + 1),
-        y: (rowIndex + 1) * 70,
-        value: values,
-        itemStyle: styles,
+
+        data.push({
+          name: node,
+          x: 90 * (inDegree + 1),
+          y: (rowIndex + 1) * fact * 70,
+          value: values,
+          itemStyle: styles,
+        });
       });
     });
-  });
-  return { data, links };
+  } catch (e) {
+    err = e;
+  }
+  return { data, links, err };
 }
-export { getDeepVec, getGraphData };
